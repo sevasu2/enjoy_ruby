f52462230832a3f11f9d70333c559d531172527a
Sevasu2 <babagame7@gmail.com>
2019-06-07 12:18:57 +0900

    配列(Array)クラス

---
+++ lesson13/list.rb
@@ -0,0 +1,16 @@
+list = ["a","b","c","d"]
+
+for i in 0..3
+	print i+1,"番目の要素は",list[i],"です。\n"
+end
+
+
+list = [1,3,5,7,9]
+sum = 0
+for i in 0..4
+	sum += list[i]
+end
+
+print "合計:",sum,"\n"
+
+
